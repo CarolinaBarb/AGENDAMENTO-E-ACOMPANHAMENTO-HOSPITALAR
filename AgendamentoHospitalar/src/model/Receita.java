3bc3f883f70398322d2169a3e9f82859530e415b
Julia Sant'Anna <juliasantanna.souza@gmail.com>
2023-01-07 11:16:44 -0300

    receita

--- AgendamentoHospitalar/src/model/Receita.java
+++ AgendamentoHospitalar/src/model/Receita.java
@@ -1,5 +1,59 @@
 package model;
 
-public class Receita {
+import java.util.Scanner;
 
-}
+public class Receita {
+		private String descricao;
+		private String PacienteAtendi;
+		private String MedicoResp;
+		
+		
+		public Receita(String descricao, String pacienteAtendi, String medicoResp) {
+			super();
+			this.descricao = descricao;
+			PacienteAtendi = pacienteAtendi;
+			MedicoResp = medicoResp;
+		}
+		
+		
+		public String getDescricao() {
+			return descricao;
+		}
+
+		public void setDescricao(String descricao) {
+			this.descricao = descricao;
+		}
+
+		public String getPacienteAtendi() {
+			return PacienteAtendi;
+		}
+
+		public void setPacienteAtendi(String pacienteAtendi) {
+			PacienteAtendi = pacienteAtendi;
+		}
+
+		public String getMedicoResp() {
+			return MedicoResp;
+		}
+
+
+		public void setMedicoResp(String medicoResp) {
+			MedicoResp = medicoResp;
+		}
+
+
+		public String EmitirReceita() {
+			Scanner in = new Scanner (System.in);
+			descricao = in.nextLine();
+			return getMedicoResp() + getPacienteAtendi() + "/n" + descricao;
+			
+		}
+		
+		
+
+	}
+
+	
+	
+	
+
