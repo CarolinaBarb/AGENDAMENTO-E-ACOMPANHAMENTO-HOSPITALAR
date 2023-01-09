e6d8b205defb7846d2eba16b1f86c36d1cf3e274
Leticia <Leticia@DESKTOP-JTM6R81>
2023-01-08 22:46:49 -0300

    Add model class

--- AgendamentoHospitalar/src/model/Prontuário.java
+++ AgendamentoHospitalar/src/model/Prontuário.java
@@ -1,5 +1,35 @@
 package model;
 
+import java.util.*;
+
 public class Prontuário {
+	private Date dtconsulta;
+	private String descricao;
+	
+	
+	public Prontuário(Date dtconsulta, String descricao) {
+		super();
+		this.dtconsulta = dtconsulta;
+		this.descricao = descricao;
+	}
+	public Date getDtconsulta() {
+		return dtconsulta;
+	}
+	public void setDtconsulta(Date dtconsulta) {
+		this.dtconsulta = dtconsulta;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	//funcao
+	public void GerarProntuario(Date dtconsulta, String descricao){
+		System.out.println("dtconsulta: "+ dtconsulta + "/n" + "descricao" + descricao);
+		
+	}
+	
 
 }
