f74eaf75a29c7a4bf34626a43232e664d8de420d
Carolina Barbosa <98655636+CarolinaBarb@users.noreply.github.com>
2023-01-09 00:36:25 -0300

    voltar aqui depois

---
+++ AgendamentoHospitalar/src/view/Login.java
@@ -0,0 +1,74 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import control.*;
+
+public class Login implements ActionListener {
+	private JFrame container;
+	private JButton botao;
+	private JTextField inserirEmail;
+	private JPasswordField inserirSenha;
+	private JLabel emailLabel;
+	private JLabel senhaLabel;
+	private JButton criarConta;
+	
+	public Login(ControlDados d){
+		container = new JFrame("Login");
+		botao = new JButton("Entrar");
+		criarConta = new JButton("Criar Conta");
+		criarConta.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		botao.setFont(new Font("Franklin Gothic Book", Font.PLAIN, 12));
+		emailLabel = new JLabel ("Email");
+		emailLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		senhaLabel = new JLabel("Senha");
+		senhaLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		criarConta.setHorizontalAlignment(SwingConstants.CENTER);
+		inserirEmail = new JTextField(25);
+		inserirSenha = new JPasswordField(25);
+		
+		container.getContentPane().setBackground(new Color(255, 228, 225));
+		container.setTitle("Login");
+		container.setSize(500, 500);
+		container.setLocation(500, 300);
+		container.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		container.getContentPane().setLayout(null);
+		
+		botao.setBounds(189,294,120,30);
+		
+		criarConta.setFont(new Font("Arial", Font.BOLD, 14));
+		criarConta.setForeground(new Color(0, 0, 0));
+		criarConta.setBounds(100, 349, 300, 30);
+		
+		inserirEmail.setBounds(100, 130, 300, 30);
+		emailLabel.setFont(new Font("Franklin Gothic Book", Font.BOLD, 16));
+		emailLabel.setBounds(100, 90, 300, 30);
+		
+		inserirSenha.setBounds(100, 210, 300, 30);
+		senhaLabel.setFont(new Font("Franklin Gothic Book", Font.BOLD, 16));
+		senhaLabel.setBounds(100, 170, 300, 30);
+		
+		container.getContentPane().add(emailLabel);
+		container.getContentPane().add(inserirEmail);
+		container.getContentPane().add(senhaLabel);
+		container.getContentPane().add(inserirSenha);
+		container.getContentPane().add(botao);
+		container.getContentPane().add(criarConta);
+		
+		container.setVisible(true);
+		
+		botao.addActionListener(this);
+		criarConta.addMouseListener(null);
+
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
