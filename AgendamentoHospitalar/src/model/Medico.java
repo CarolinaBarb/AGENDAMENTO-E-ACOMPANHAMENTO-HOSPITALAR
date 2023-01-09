dd38d105b7c8b2de4067c480b95c256c1576347e
Julia Sant'Anna <juliasantanna.souza@gmail.com>
2023-01-09 19:28:19 -0300

    arrumar

--- AgendamentoHospitalar/src/model/Medico.java
+++ AgendamentoHospitalar/src/model/Medico.java
@@ -4,62 +4,62 @@ import java.util.*;
 
 
 public class Medico extends Pessoa {
-	private String crm;
-	private String especialidade;
-	private String Convenios_aceitos;
-	
-	//construtor
+    private String crm;
+    private String especialidade;
+    private String Convenios_aceitos;
 
-	public Medico(String nome, String id, String sexo, Date dataNascimento, String crm, String especialidade,
-			String convenios_aceitos) {
-		super(nome, id, sexo, dataNascimento);
-		this.crm = crm;
-		this.especialidade = especialidade;
-		this.Convenios_aceitos = convenios_aceitos;
-	}
+    //construtor
+
+    public Medico(String nome,String email, String senha, String id, String sexo, Date dataNascimento, String crm, String especialidade,
+            String convenios_aceitos) {
+        super(nome, email, senha, id, sexo, dataNascimento);
+        this.crm = crm;
+        this.especialidade = especialidade;
+        this.Convenios_aceitos = convenios_aceitos;
+    }
 
 
-	//getters and setters
+    //getters and setters
 
-	public String getCrm() {
-		return crm;
-	}
+    public String getCrm() {
+        return crm;
+    }
 
-	public void setCrm(String crm) {
-		this.crm = crm;
-	}
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
 
-	public String getEspecialidade() {
-		return especialidade;
-	}
+    public String getEspecialidade() {
+        return especialidade;
+    }
 
-	public void setEspecialidade(String especialidade) {
-		this.especialidade = especialidade;
-	}
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
 
-	public String getConvenios_aceitos() {
-		return Convenios_aceitos;
-	}
+    public String getConvenios_aceitos() {
+        return Convenios_aceitos;
+    }
 
-	public void setConvenios_aceitos(String convenios_aceitos) {
-		Convenios_aceitos = convenios_aceitos;
-	}
-	
-	//funçao
-	
-	public void ObterConvenios() {
-		int quantidade = 0;
-		System.out.println("digite a quantidade de convenios: ");
-		Scanner in = new Scanner(System.in);
-		quantidade = in.nextInt();
-		
-		for (int i = 0; i <= quantidade; i++) {
-			System.out.println("digite o nome do convenio aceito: ");
-			Convenios_aceitos = in.nextLine();
-			System.out.println(i + Convenios_aceitos);
-		}
-		
-		
-	}
+    public void setConvenios_aceitos(String convenios_aceitos) {
+        Convenios_aceitos = convenios_aceitos;
+    }
+
+    //funçao
+
+    public void ObterConvenios() {
+        int quantidade = 0;
+        System.out.println("digite a quantidade de convenios: ");
+        Scanner in = new Scanner(System.in);
+        quantidade = in.nextInt();
+
+        for (int i = 0; i <= quantidade; i++) {
+            System.out.println("digite o nome do convenio aceito: ");
+            Convenios_aceitos = in.nextLine();
+            System.out.println(i + Convenios_aceitos);
+        }
+
+
+    }
 
 }
--- AgendamentoHospitalar/src/model/Paciente.java
+++ AgendamentoHospitalar/src/model/Paciente.java
@@ -4,54 +4,36 @@ package model;
 import java.util.*;
 
 public class Paciente extends Pessoa{
-	private String cpf;
-	private double altura;
-	private double peso;
-	private String observacao;
-	//construtor
-	public Paciente(String nome, String id, String sexo, Date dataNascimento, String cpf, double altura, double peso,
-			String observacao) {
-		super(nome, id, sexo, dataNascimento);
-		this.cpf = cpf;
-		this.altura = altura;
-		this.peso = peso;
-		this.observacao = observacao;
-	}
-	//getters and setters
-	public String getCpf() {
-		return cpf;
-	}
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	public double getAltura() {
-		return altura;
-	}
-	public void setAltura(double altura) {
-		this.altura = altura;
-	}
-	public double getPeso() {
-		return peso;
-	}
-	public void setPeso(double peso) {
-		this.peso = peso;
-	}
-	public String getObservacao() {
-		return observacao;
-	}
-	public void setObservacao(String observacao) {
-		this.observacao = observacao;
-	}
-	//funções
-	public void GerarObservacao() {
-		System.out.println("Por favor escrever observacao: ");
-		Scanner in = new Scanner(System.in);
-		observacao = in.nextLine();
-	}
-	
-	
-	
-	
-	
-
+    private String cpf;
+    private double altura;
+    private double peso;
+    private String observacao;
+    //construtor
+    public Paciente(String nome,String email, String senha, String id, String sexo, Date dataNascimento, String cpf, double altura, double peso,
+            String observacao) {
+        super(email, senha, sexo, nome, id, dataNascimento);
+        this.cpf = cpf;
+        this.altura = altura;
+        this.peso = peso;
+        this.observacao = observacao;
+    }
+    //getters and setters
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    public double getPeso() {
+        return peso;
+    }
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
 }
