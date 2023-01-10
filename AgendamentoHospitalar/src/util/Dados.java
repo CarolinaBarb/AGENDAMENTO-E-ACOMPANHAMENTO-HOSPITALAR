0d66cf51d39f8f32f5735cd19315ddcd05d6278d
Carolina Barbosa <carolinabarbosab09@gmail.com>
2023-01-10 19:19:56 -0300

    corrigindo erro em controldados

--- AgendamentoHospitalar/src/util/Dados.java
+++ AgendamentoHospitalar/src/util/Dados.java
@@ -8,5 +8,30 @@ public class Dados {
 	
 	
 	public static ArrayList<Pessoa> usuarios = new ArrayList <Pessoa>();
+
+	public void dados() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Pessoa[] getpessoa() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getQuantidadepessoa() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void setQuantidadepessoa(int quantidadePessoa) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setpessoa(Pessoa[] pessoa) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
