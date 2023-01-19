93cb2a77eba40596cf75aaf37961ef44c50eec2a
julia Sant'Anna <juliasantanna.souza@gmail.com>
2023-01-18 22:07:46 -0300

    completando model consulta

--- AgendamentoHospitalar/src/model/Consulta.java
+++ AgendamentoHospitalar/src/model/Consulta.java
@@ -7,7 +7,9 @@ public class Consulta {
 	private double valor;
 	private Medico idMedico;
 	private Paciente idPaciente;
-	//construtores
+	private String data;
+
+	// construtores
 	public Consulta(String especialidade, double valor, Medico idMedico, Paciente idPaciente) {
 		super();
 		this.especialidade = especialidade;
@@ -15,39 +17,52 @@ public class Consulta {
 		this.idMedico = idMedico;
 		this.idPaciente = idPaciente;
 	}
-	
-	
+
 	public Consulta(String especialidade, double valor) {
 		super();
 		this.especialidade = especialidade;
 		this.valor = valor;
 	}
 
-
-	//getters and setters
+	// getters and setters
 	public String getEspecialidade() {
 		return especialidade;
 	}
+
 	public void setEspecialidade(String especialidade) {
 		this.especialidade = especialidade;
 	}
+
 	public double getValor() {
 		return valor;
 	}
+
 	public void setValor(double valor) {
 		this.valor = valor;
 	}
+
 	public Medico getIdMedico() {
 		return idMedico;
 	}
+
 	public void setIdMedico(Medico idMedico) {
 		this.idMedico = idMedico;
 	}
+
 	public Paciente getIdPaciente() {
 		return idPaciente;
 	}
+
 	public void setIdPaciente(Paciente idPaciente) {
 		this.idPaciente = idPaciente;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
 	}
 
 }
